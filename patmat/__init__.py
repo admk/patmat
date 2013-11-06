bed8473aa0efc41d33adb11f995af604b5f7cbca
Xitong Gao <gxtfmx@gmail.com>
2013-11-06 01:30:01 +0000

    Release 1.0.1

--- patmat/__init__.py
+++ patmat/__init__.py
@@ -2,7 +2,7 @@
 .. pakage:: patmat
     :synopsis: Functional programming style pattern matching in Python.
 """
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 __author__ = 'Xitong Gao'
 __email__ = '@'.join(['gxtfmx', 'gmail.com'])
 __license__ = 'MIT'
